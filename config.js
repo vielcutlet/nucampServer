f175ce98f38fcdd9e5f9dedd127dfa98fdb07533
Daniel Viel <vielcutlet@gmail.com>
2021-02-19 15:35:42 -0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '254479429587702',
+        clientSecret: '9cbfd51bae9af5cc5e64b39732d03804'
+    }
 }
